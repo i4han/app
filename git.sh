4e739281ded4aed2e2496be142f1103101a281e0
i4han <isaac@hi16.ca>
2014-05-03 01:56:23 +0000

    git

--- git.sh
+++ git.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 cd ~/workspace
+cake profile
 . profile
 git add --all
 git commit -m $1
+if [ -n "$(git status --porcelain)" ]; then 
+  echo "there are changes"; 
+else 
+  echo "no changes";
+fi
 git push
 cd bin
 git add --all
