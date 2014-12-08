30861875498554ee9a393175e91b9771a63046cd
i4han <isaac@hi16.ca>
2014-12-08 17:42:17 +0000

    install and startup

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-npm install --prefix ~/node_modules npm
-
 git submodule update --init
 
 for i in redis meteor
@@ -11,7 +9,18 @@ done
 
 [ -d ~/node_modules ] || mkdir ~/node_modules
 
+npm install --prefix ~/node_modules npm
+read -n1 -r -p "Press any key to install npm packages." key
 node npm_packs
 
+read -n1 -r -p "Creating profile." key
+if [ ! -e profile ]; then
+    $HOME/node_modules/.bin/cake profile
+    . profile
+    cake config profile
+fi
+. profile
+
+[ "x"`redis-cli ping` == "xPONG" ] || parts start redis
 
 > ~/.installed
--- startup.sh
+++ startup.sh
@@ -4,11 +4,6 @@ echo "Starting up..."
 [ -e ~/.installed ] || . install.sh
 [ "x"`redis-cli ping` == "xPONG" ] || parts start redis
 
-if [ ! -e profile ]; then
-    $HOME/node_modules/.bin/cake profile
-    . profile
-    cake config profile
-fi
 . profile
 
 for i in client lib private
