7d3b7e18a721215acd7e0d1bc08cc1d3446b6361
i4han <isaac@hi16.ca>
2014-12-08 18:28:16 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,14 @@ git submodule update --init
 
 for i in redis meteor
 do
-    [[ `parts list` == *"$i"* ]] || parts install $i
+    [[ `parts list` =~ $i ]] || parts install $i
 done
 
 [ -d ~/node_modules ] || mkdir ~/node_modules
 
-read -n1 -r -p "Installing npm." key
 npm install --prefix ~/node_modules npm
-read -n1 -r -p "Installing npm packages." key
 node npm_packs
 
-read -n1 -r -p "Creating profile." key
 if [ ! -e profile ]; then
     $HOME/node_modules/.bin/cake profile
     . profile
