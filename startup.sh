42af9447d39827f05be037122230c145a596485d
i4han <isaac@hi16.ca>
2014-12-09 02:42:23 +0000

    startup

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Starting up..."
-[ -e ~/.installed ] || echo "Install.sh first." && exit 0
+[ -e ~/.installed ] || ( echo "Install.sh first." && exit 0 )
 [ "x"`redis-cli ping` == "xPONG" ] || parts start redis
 . profile
 cake watch
